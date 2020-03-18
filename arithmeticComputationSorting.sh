15f223b5a0619bf582317300bde46b63577beb1b
sagar-528 <gupta.sagar528@gmail.com>
2020-03-18 15:59:54 +0530

    ADDED: Three Input Code

---
+++ arithmeticComputationSorting.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+#->Problem Statement:
+#->Author: Sagar Gupta 
+#->Date: Mar 2020 
+
+read -p "Enter first number :" a
+read -p "Enter second number :" b
+read -p "Enter third number :" c
+
