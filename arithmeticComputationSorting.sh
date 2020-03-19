754c54390d2d1d7b233684b2dead8bf3aa7f8421
sagar-528 <gupta.sagar528@gmail.com>
2020-03-19 20:08:01 +0530

    MODIFIED: COmments

--- arithmeticComputationSorting.sh
+++ arithmeticComputationSorting.sh
@@ -3,18 +3,18 @@
 #->Author: Sagar Gupta 
 #->Date: Mar 2020 
 
-#reading the value
+#Reading the value
 read -p "Enter first number :" number1
 read -p "Enter second number :" number2
 read -p "Enter third number :" number3
 
-#computing problems
+#Computing problems
 result1=`echo "scale=3; $number1+$number2*$number3" | bc`
 result2=`echo "scale=3; $number1*$number2+$number3" | bc`
 result3=`echo "scale=3; $number3+$number1/$number2" | bc`
 result4=`echo "scale=3; $number1%$number2+$number3" | bc`
 
-#storing results in dictionary
+#Storing results in dictionary
 arithmeticDictionary[1]=$result1
 arithmeticDictionary[2]=$result2
 arithmeticDictionary[3]=$result3
@@ -26,7 +26,7 @@ do
 	array[value]="${arithmeticDictionary[$value]}"
 done
 
-#result in descending order
+#Result in descending order
 echo "Sorted array in descending order"
 for counter1 in ${!array[@]}
 do
@@ -43,7 +43,7 @@ done
 
 echo "${array[@]}"
 
-#result in ascending order
+#Result in ascending order
 echo "Sorted array in ascending order"
 for counter1 in ${!array[@]}
 do
